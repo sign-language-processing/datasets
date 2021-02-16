e53d16cc8842d15441eb83e3c54d901f23f2e4f4
Amit <amitmoryossef@gmail.com>
2021-02-16 22:09:40 +0200

    fix(autsl): don't load val split if only annotations

--- sign_language_datasets/datasets/autsl/autsl.py
+++ sign_language_datasets/datasets/autsl/autsl.py
@@ -150,16 +150,16 @@ class AUTSL(tfds.core.GeneratorBasedBuilder):
 
     splits = [
       tfds.core.SplitGenerator(
-        name=tfds.Split.VALIDATION,
-        gen_kwargs={"videos_path": valid_videos, "poses_path": valid_pose_path, "labels_path": valid_labels}
+        name=tfds.Split.TRAIN,
+        gen_kwargs={"videos_path": train_videos, "poses_path": train_pose_path, "labels_path": train_labels}
       )
     ]
 
     # If no validation set, no data even about its annotations
-    if valid_videos is not None or valid_pose_path is not None:
+    if valid_videos is not None or valid_pose_path is not None or valid_labels is not None:
       splits.append(tfds.core.SplitGenerator(
-        name=tfds.Split.TRAIN,
-        gen_kwargs={"videos_path": train_videos, "poses_path": train_pose_path, "labels_path": train_labels}
+        name=tfds.Split.VALIDATION,
+        gen_kwargs={"videos_path": valid_videos, "poses_path": valid_pose_path, "labels_path": valid_labels}
       ))
 
     return splits
