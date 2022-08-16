c6cd2f136602eceb5ac2dc1368ca5edfd081ec7e
Amit Moryossef <amitmoryossef@gmail.com>
2022-08-16 15:27:47 +0200

    feat(license): change text

--- sign_language_datasets/datasets/warning.py
+++ sign_language_datasets/datasets/warning.py
@@ -2,4 +2,5 @@ import warnings
 
 
 def dataset_warning(dataset):
-    warnings.warn("Users who download data are responsible for checking the license of data sets!")
+    warnings.warn("This library provides access to data sets without claiming ownership over them or defining their licensing terms. "
+                  "Users who download data are responsible for checking the license of each individual data set.")
